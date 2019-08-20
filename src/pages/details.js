b585db05ee647a2846b94f0eac6e883719142544
brianbinder <bbinder912@gmail.com>
2019-08-19 21:13:43 -0400

    Add welcome drinks to details page

--- src/pages/details.js
+++ src/pages/details.js
@@ -7,6 +7,22 @@ const IndexPage = () => (
     <h1 className="page-header">Details</h1>
     <div className="page-content details">
       <section>
+        <h2>Welcome Drinks</h2>
+        <p><span>Friday, October 25, 2019</span></p>
+        <p><span>7:30pm</span></p>
+      </section>
+      <section>
+        <p><span>Courtyard Marriott Dulles</span></p>
+        <p><span>Town Center</span></p>
+        <p><span>45500 Majestic Dr.</span></p>
+        <p><span>Dulles, VA 20166</span></p>
+      </section>
+      <section>
+        <p><span>Come say hi! Everyone is invited for</span></p>
+        <p><span>drinks with the bride and groom.</span></p>
+      </section>
+      <br />
+      <section>
         <h2>Ceremony</h2>
         <p><span>Saturday, October 26, 2019</span></p>
         <p><span>5:30pm</span></p>
